d958df1672104346ae63dd3a05ab4cd5761bddf0
Vinicius-11340 <vinicius_zakalusny@estudante.sesisenai.org.br>
2025-10-07 12:49:24 -0300

    Conexão

--- arduino_vini/arduino_vini.ino
+++ arduino_vini/arduino_vini.ino
@@ -1,6 +1,17 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
+
+WifiClient client;
+PubSubClient mqtt(client);
+
 const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
+
+const String brokerURL = "test.mosquitto.org";
+const int brokerPort = ;
+
+const String brokerUser = "";
+const String brokerPass = "";
 
 void setup() {
   Serial.begin(115200); //configura a placa pra mostrar na tela
@@ -12,9 +23,23 @@ void setup() {
   }
     Serial.println("Conectado com Sucesso parceiro!");
 
+  mqtt.setServer(brokerURL.c_str(),brokerPort);
+  Serial.println("Conectando no Broker");
+
+  Serial boardID = "S1-conexão";
+  boardID += String(random(0xffff), HEX); 
+
+  while(!mqtt.connect(boardID.c_str( ))){
+    Serial.print(".");
+    delay(200);
+  }
+    mqttClient.subscribe("AulaIoT/msg");
+  Serial.println("\nConectado com sucesso ao broker"!);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  String msg = "Vinicius: Oi";
+  String topico = "AulaIoT/msg";
+  mqtt.publish(topico.c_str(), msg.c_str());
+  delay(2000); 
 }
